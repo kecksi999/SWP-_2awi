8b746fdf1fe2d7b0134190bba6adf26363912b9d
Jan <jan.keckeis@hotmail.com>
2024-01-15 11:08:18 +0100

    finished Aufgabe zu Variablen

---
+++ JS/Aufgabe zu Variablen/script.js
@@ -0,0 +1,22 @@
+let banana = "Banane";
+let apple = "Apple";
+let bananaPricePerKilo = 2.14;
+let applePricePerKilo = 3.43;
+
+let appleWeight = 0.34;
+let bananaWeight = 0.22;
+
+let ApplePerKilo = 1 / appleWeight;
+let BananaPerKilo = 1 / bananaWeight;
+
+let PricePerApple = applePricePerKilo * appleWeight;
+let PricePerBanana = bananaPricePerKilo * bananaWeight;
+
+console.log("Der Preis von einem Kilo Äpfel beträgt " + ApplePerKilo);
+console.log("Der Preis von einem Kilo Bananen beträgt " + BananaPerKilo);
+console.log("Der Preis von einem Apfel beträgt " + PricePerApple);
+console.log("Der Preis von einer Banane beträgt " + PricePerBanana);
+console.log("Der Preis von einer Tonne Äpfel beträgt " + (ApplePerKilo * 1000));
+console.log("Der Preis von einer Tonne Bananen beträgt " + (BananaPerKilo * 1000));
+console.log("Preis von 8 Äpfeln ist " + (PricePerApple * 8));
+console.log("Preis von 17 Bananen ist " + (PricePerBanana * 17));
--- Test 20240108/script.js
+++ Test 20240108/script.js
@@ -1,16 +1,16 @@
 let countLessThanOrEqual12 = 0;
-let countGreaterThan12 = 0;
+let countGreaterThan12 = 0;      // zählt die zahlen die kleiner bzw größer als 12 sind
 
 for (let i = 0; i < 10; i++) {
-  const randomNumber = Math.floor(Math.random() * 11) + 5;
+  const randomNumber = Math.floor(Math.random() * 11) + 5;   // sozusagen zufallsgenerator von 5 bis 15
 
   if (randomNumber > 12) {
     console.log(randomNumber);
     countGreaterThan12++;
   } else {
-    countLessThanOrEqual12++;
+    countLessThanOrEqual12++; // ++ steht dafür das es immer 1 dazu addiert 
   }
 }
 
 console.log("Anzahl der Zahlen kleiner-gleich 12:", countLessThanOrEqual12);
-console.log("Anzahl der Zahlen größer als 12:", countGreaterThan12);
+console.log("Anzahl der Zahlen größer als 12:", countGreaterThan12);            // console.log zeigt in der konsole wie viele zahlen größer und kleiner- gleich sind.
