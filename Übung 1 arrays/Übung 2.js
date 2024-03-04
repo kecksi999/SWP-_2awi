c90be4aef4cfec253cd99d16a146edbe4ec91560
Jan <jan.keckeis@hotmail.com>
2024-03-04 11:25:30 +0100

    Übung 2

---
+++ Übung 1 arrays/Übung 2.js
@@ -0,0 +1,12 @@
+function invertiereBitfolge(bitfolge) {
+    var invertiert = "";
+    for (var x = 0; x < bitfolge.length; i++) {
+        invertiert += (bitfolge[x] === '0') ? '1' : '0';
+    }
+    return invertiert;
+}
+
+
+var input = "000111000";
+var output = invertiereBitfolge(input);
+console.log(output); 
